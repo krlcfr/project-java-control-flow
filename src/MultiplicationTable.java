4fac0f37cd2c42027137c7a25c813ad4acef9cdb
Santiago <santiago.gallego28@outlook.com>
2025-11-07 16:37:28 -0500

    feat: Trabajando con Bucles For

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println("Multiplication table for " + number + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+    }
+}
